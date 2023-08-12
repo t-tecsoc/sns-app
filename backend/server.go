a4532b2be342b60f4302c41c36b61f2d68398070
t-tecsoc <25559838+t-tecsoc@users.noreply.github.com>
2023-08-13 04:55:36 +0900

    作り直し

---
+++ backend/server.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	// Replace username with your github username
+	"backend/graph"
+	"context"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+)
+
+// Defining the Graphql handler
+func graphqlHandler() gin.HandlerFunc {
+	// NewExecutableSchema and Config are in the generated.go file
+	// Resolver is in the resolver.go file
+	h := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+// Defining the Playground handler
+func playgroundHandler() gin.HandlerFunc {
+	h := playground.Handler("GraphQL", "/query")
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+func GinContextToContextMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx := context.WithValue(c.Request.Context(), "GinContextKey", c)
+		c.Request = c.Request.WithContext(ctx)
+		c.Next()
+	}
+}
+
+func main() {
+	// Setting up Gin
+	r := gin.Default()
+	r.Use(GinContextToContextMiddleware())
+	r.GET("/", playgroundHandler())
+
+	if gin.Mode() != gin.ReleaseMode {
+		r.POST("/query", graphqlHandler())
+	}
+
+	r.Run()
+}
